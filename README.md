9356d20bcba69519935b67cc4f24479eb26c0db6
darkstego <darkstego@gmail.com>
2018-04-07 05:35:23 -0400

    Updated Readme and attached mode.el

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # wakib-mode
 Emacs mode that moves to modern keybindings
+
+This mode that in my humble opinion is needed to give Emacs some
+modern shortcuts.
+
+
+## Installation
+
+Just copy the wakib-mode.el file to the location in your emacs loadpath
+and put
+
+```
+(require 'wakib-mode)
+(wakib-mode 1)
+```
+in your init.el file
+
+## Features
+
+Proper CUA editor
+
+Moved the common Emacs prefixes to free up key for proper copy-cut shortcuts
+
+The new prefixes are
+
+C-c => C-d
+C-x => C-e
+
+This moves the prefixes to two keys that to my knowledge aren't commonly mapped
+and can be reached very easily.
+
+Please note that only the prefix key changes in emacs commands, so the vanilla
+Emacs C-c C-c press becomes C-d C-c in this mode.
+
+This minor mode is heavily influenced by Ergoemacs mode. So movement is by
+using Alt + ijkl keys. I hope to finish up the keybindings soon. This is
+modifier based mode. If you are looking for a modal keybinding in the same
+vein, check out xah-fly-keys.
+
+Much thanks to the general.el project for giving me the solution of
+dynamically binding prefix keys. If you simply want to copy the C-c key
+to another location, while keeping C-c as is, check them out, they provide a
+an easy way of doing it. Or just check out the Macro shown in my mode.
+
+## Warnings
+
+The C-c key doesn't return to normal after you turn of wakib mode. You need
+to run
+```
+(setq wakib-cc-mode nil)
+```
+to get it back. I will bind this the mode-hook on mode disable once I figure
+it out.
+
+It seems C-x is overwritten on certain occasions due to minor modes actually
+using it (who knew). I will implement the same override for C-x as I did for
+C-c to handle those.
+
+I am very very new to elisp, and emacs so any suggestions or advice is
+greatly appreciated.
---
+++ wakib-mode.el
@@ -0,0 +1,149 @@
+(require 'general)
+
+
+;; Functions & Macros
+
+(cl-defmacro wakib-cc-key ()
+  "Act as C-c definition in the current context.
+This uses an extended menu item's capability of dynamically computing a
+definition. This idea came from general.el"
+  `'(menu-item
+     ,""
+     nil
+     :filter
+     (lambda (&optional _)
+		 ,`(wakib-get-all-keymaps))))
+
+
+;; use let instead of double call to (car x)
+(defun wakib-minor-cc-keymaps()
+  (let ((active-maps nil))
+	 (mapc (lambda (x) 
+				(when (and (symbolp (car x)) (symbol-value (car x)))
+				  (add-to-list 'active-maps  (lookup-key (cdr x) (kbd "C-c")))))
+			 minor-mode-map-alist )
+	 (make-composed-keymap active-maps)))
+
+
+
+;; might need to do keymap inheretence to perserve priority
+(defun wakib-get-all-keymaps ()
+  (make-composed-keymap (list (wakib-minor-cc-keymaps) (local-key-binding (kbd "C-c")) (global-key-binding (kbd "C-c")))))
+
+
+;; Commands
+
+(defun wakib-new-empty-buffer ()
+  "Create a new empty buffer and switch to it.
+New buffer will be named “untitled” or “untitled<2>”, etc.
+
+It returns the buffer (for elisp programing)."
+  (interactive)
+  (let (($buf (generate-new-buffer "untitled")))
+	 (switch-to-buffer $buf)
+	 (funcall initial-major-mode)
+	 (setq buffer-offer-save t)
+	 $buf))
+
+(defun wakib-insert-newline-before ()
+  "Insert a newline and indent before current line"
+  (interactive)
+  (move-beginning-of-line)
+  (newline-and-indent)
+  (previous-line)
+  (indent-for-tab-command))
+
+
+(defun wakib-close-current-buffer ()
+  "kill current buffer"
+  (interactive)
+  (kill-buffer (current-buffer)))
+
+
+
+;; Setup for keymap
+;; I should probably move this into a let statement,
+;; no need expose any of these functions or variables
+;; except for wakib-mode-map
+
+(defvar wakib-mode-map (make-sparse-keymap) "Key bindings for Wakib minor mode")
+
+(defun wakib-define-keys (keymap keylist)
+  "Map over alist (keylist) and add to keymap"
+  (interactive)
+  (mapc (lambda (pair)
+          (define-key keymap (kbd (car pair)) (cdr pair)))
+        keylist))
+
+
+;; Need to update on hook when mode disabled
+(defun wakib-update-cc-override ()
+  (setq wakib-cc-mode wakib-mode))
+(add-hook 'wakib-mode-hook 'wakib-update-cc-override)
+
+
+(defvar wakib-cc-mode-map (make-sparse-keymap))
+(define-key wakib-cc-mode-map (kbd "C-c") 'kill-ring-save)
+(defun wakib-cc-override ()
+  "Add modemap to override C-c into minor-mode-overriding-map-alist"
+  (interactive)
+  (add-to-list 'minor-mode-overriding-map-alist (cons 'wakib-cc-mode wakib-cc-mode-map)))
+(add-hook 'after-change-major-mode-hook 'wakib-cc-override)
+
+
+(defvar wakib-keylist
+  '(("M-j" . backward-char)
+	 ("M-l" . forward-char)
+	 ("M-i" . previous-line)
+	 ("M-k" . next-line)
+	 ("M-u" . backward-word)
+	 ("M-o" . forward-word)
+	 ("M-h" . beginning-of-line)
+	 ("M-;" . end-of-line)
+	 ("C-n" . wakib-new-empty-buffer)
+	 ("C-o" . find-file)
+	 ("C-w" . wakib-close-current-buffer)
+	 ("C-<next>" . next-buffer)
+	 ("C-<prior>" . previous-buffer) 
+	 ("C-x" . kill-region)
+	 ("C-v" . yank)
+	 ("C-z" . undo)
+	 ("C-f" . isearch-forward)
+	 ("C-s" . save-buffer)
+	 ("C-p" . print-buffer)
+	 ("C-=" . text-scale-increase)
+	 ("C--" . text-scale-decrease)
+	 ("M-e" . backward-kill-word)
+	 ("M-r" . kill-word)
+	 ("M-a" . execute-extended-command)
+	 ("M-<f4>" . save-buffers-kill-terminal)
+	 ("M-d" . delete-backward-char)
+	 ("M-f" . delete-char)
+	 ("M-SPC" . set-mark-command)
+	 ("M-RET" . wakib-insert-newline-before)
+	 ("C-b" . switch-to-buffer)
+	 ("<escape>" . keyboard-escape-quit)) ;; doesn't work well, check ergoemacs
+  "List of all wakib mode keybindings")
+  
+  
+(wakib-define-keys wakib-mode-map wakib-keylist)
+
+(define-key wakib-mode-map (kbd "C-e") ctl-x-map)
+(define-key wakib-mode-map (kbd "C-d") (wakib-cc-key))
+
+
+(define-minor-mode wakib-mode
+  "This mode brings modern style keybindings to Emacs.
+Major changes is proper CUA key bindings by moving C-c and C-x to
+C-d and C-e respectively. This allow access to all the keybindings of
+Emacs while not tripping up users who do not want a steep learning curve
+just to use their editor.
+
+Note that only the first prefix is changed. So C-c C-c becomes C-d C-c."
+  :lighter " Wakib"
+  :keymap wakib-mode-map
+  :init-value t
+  :after-hook (setq wakib-cc-mode 1))
+
+
+(provide 'wakib-mode)

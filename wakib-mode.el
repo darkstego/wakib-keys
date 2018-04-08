3f0967e2e0563becf2babfae4f885fde7569ea52
darkstego <darkstego@gmail.com>
2018-04-08 07:34:24 -0400

    Cleaned up comments to work with package.el

--- wakib-mode.el
+++ wakib-mode.el
@@ -9,6 +9,10 @@
 
 ;; This file is not part of GNU Emacs.
 
+;;; Commentary:
+
+;; Emacs minor mode that provides a modern, efficient and easy to learn keybindings
+
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
@@ -22,7 +26,7 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-;; Code
+;;; Code:
 
 ;; Functions & Macros
 
@@ -269,3 +273,6 @@ Note that only the first prefix is changed. So C-c C-c becomes C-d C-c."
 
 
 (provide 'wakib-mode)
+
+
+;;; wakib-mode.el ends here
